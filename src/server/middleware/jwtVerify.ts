03ffcc2b588a4a9b070d8d2a11037412833d5c72
hy2n <25565k@daum.net>
2024-09-08 00:14:29 +0900

    bug : 관리자 JWT인증 문제 수정 잘못된 변수명 수정

--- src/server/middleware/jwtVerify.ts
+++ src/server/middleware/jwtVerify.ts
@@ -28,12 +28,11 @@ export const ADMIN_PROTECT = (req: Request, res: Response, next: NextFunction) =
     }
 
     const token = authroize_header.split(' ')[1];
-
     try {
         const decoded = jwt.verify(token, JWT_KEY) as jwtPayload
         req.params.user_email = decoded.user_email
-        
-        if (req.query.__email != ADMIN_EMAIL) {
+
+        if (req.params.user_emaill != ADMIN_EMAIL) {
             return res.status(401).json({ message: 'unauthorized ㅗ' })
         }
         next();
