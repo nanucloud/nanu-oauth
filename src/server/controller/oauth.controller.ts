1e968665ff57edea6285900cfcb557d6d2cf548d
hy2n <github1@ncloud.sbs>
2024-09-19 17:08:09 +0900

    hotfix : Token을 Bearer으로 잡게 수정

--- src/server/controller/oauth.controller.ts
+++ src/server/controller/oauth.controller.ts
@@ -125,7 +125,13 @@ const isValidDomain = (url: string) => { // 도메인 검증
 
 export const clientAuthValidation = async (req: Request, res: Response) => {
     const { ClientId } = req.params;
-    const { access_token } = req.body;
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Authorization header missing or invalid' });
+    }
+
+    const access_token = authHeader.split(' ')[1];
 
     try {
         const application = await applicationRepository.findOne({ where: { client_key: ClientId } });
