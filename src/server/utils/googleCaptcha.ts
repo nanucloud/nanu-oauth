2e6fc3a8cee3afa6636aad3aa92d01c7d67c95d2
hy2n <github1@ncloud.sbs>
2024-10-20 17:25:33 +0900

    fix : google Capcha 인증 엔드포인트 수정

--- src/server/utils/googleCaptcha.ts
+++ src/server/utils/googleCaptcha.ts
@@ -2,26 +2,13 @@ import axios from 'axios';
 import { CAPTCHA_KEY } from '../config/systemkeys';
 
 export const RecaptchaVerify = async function (token: string): Promise<boolean> {
-  const url = `https://www.google.com/recaptcha/api/siteverify`;
   try {
-    const response = await axios.post(url, {
-      event: {
-        token: token,
-        secret: CAPTCHA_KEY
-      }
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-
-    return response.data.success && response.data.score >= 0.5;
+    const response = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${CAPTCHA_KEY}&response=${token}`
+    );
+    return response.data.success;
   } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      console.error('Error details:', error.response.data.error);
-    } else {
-      console.error('An unexpected error occurred:', error);
-    }
-    return false
+    console.error('reCAPTCHA verification failed:', error);
+    return false;
   }
 }
